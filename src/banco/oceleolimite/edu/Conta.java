8c43b2e5d77a7b4266fc9ee39c68bf6f8c4e592a
Lucrecio Paes <lcpaes40@gmail.com>
2022-11-30 15:05:16 -0300

    Intruções criadas para metodos na classe Conta

--- src/banco/oceleolimite/edu/Conta.java
+++ src/banco/oceleolimite/edu/Conta.java
@@ -1,10 +1,16 @@
 package banco.oceleolimite.edu;
 
 public abstract class Conta implements IConta {
-    private int agencia;
-    private int numero;
-    private double saldo;
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
 
+    public Conta() {
+        this.agencia + AGENCIA_PADRAO;
+        this.numero + SEQUENCIAL++;
+    }
     @Override
     public void depositar(double valor) {
 
